751464693d8e2c7692e4ee43d2280cb647d67aa1
Jen Wachter <jenleighkelly@gmail.com>
2014-03-27 13:02:38 -0400

    Created createRepeater() method.

--- src/WPUtilities/ACF/Repeater.php
+++ src/WPUtilities/ACF/Repeater.php
@@ -4,6 +4,12 @@ namespace WPUtilities\ACF;
 
 class Repeater
 {
+    /**
+     * Take metadata from a WordPress post and clean up
+     * the ACF repeater fields
+     * @param  array $meta Metadata
+     * @return array Cleaned up metadata
+     */
     public function cleanMeta($meta)
     {
         // potential ACF fields based on integer value
@@ -44,6 +50,54 @@ class Repeater
         return $meta;
     }
 
+    public function createRepeater($array = array())
+    {
+        // $example = array(
+        //     "images" => array(
+        //         array(
+        //             "type" => "low_resolution",
+        //             "utl" => "http://distilleryimage4.s3.amazonaws.com/df3860c2b3bd11e3bd391271df4caaa4_6.jpg"
+        //         ),
+        //         array(
+        //             "type" => "thumbnail",
+        //             "utl" => "http://distilleryimage4.s3.amazonaws.com/df3860c2b3bd11e3bd391271df4caaa4_5.jpg"
+        //         ),
+        //         array(
+        //             "type" => "standard_resolution",
+        //             "utl" => "http://distilleryimage4.s3.amazonaws.com/df3860c2b3bd11e3bd391271df4caaa4_8.jpg"
+        //         )
+        //     )
+        // );
+
+        $meta = array();
+
+        foreach ($array as $fieldName => $subfieldData) {
+
+            $num = 0;
+
+            foreach ($subfieldData as $subfields) {
+
+                $meta[$fieldName] = count($subfieldData);
+
+                foreach ($subfields as $k => $v) {
+                    $meta["{$fieldName}_{$num}_{$k}"] = $v;
+                }
+
+                $num++;
+                
+            }
+
+        }
+
+        return $meta;
+
+    }
+
+    /**
+     * Find meta keys whose values are integers
+     * @param  array $meta Metadata
+     * @return array Keys
+     */
     protected function findIntegerValues($meta)
     {
         $ints = array();
--- tests/WPUtilities/ACF/RepeaterTest.php
+++ tests/WPUtilities/ACF/RepeaterTest.php
@@ -72,4 +72,31 @@ class RepeaterTest extends \tests\Base
         $result = $this->testClass->cleanMeta($given);
         $this->assertEquals($expected, $result);
     }
+
+    public function testCreateRepeater()
+    {
+        $given = array(
+            "images" => array(
+                array(
+                    "type" => "low_resolution",
+                    "url" => "a url"
+                ),
+                array(
+                    "type" => "thumbnail",
+                    "url" => "another url"
+                )
+            )
+        );
+
+        $expected = array(
+            "images" => 2,
+            "images_0_type" => "low_resolution",
+            "images_0_url" => "a url",
+            "images_1_type" => "thumbnail",
+            "images_1_url" => "another url"
+        );
+
+        $result = $this->testClass->createRepeater($given);
+        $this->assertEquals($expected, $result);
+    }
 }
