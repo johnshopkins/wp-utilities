89067c098e805de267d4425b26cf261f78967521
Jen Wachter <jenleighkelly@gmail.com>
2014-05-19 15:11:11 -0400

    Finished up first round of transformers.

--- src/Theme/Transformers/Base.php
+++ src/Theme/Transformers/Base.php
@@ -10,15 +10,90 @@ class Base
    */
   protected $api;
 
+  protected $contentTypes;
+
+  /**
+   * Field types whose data needs
+   * to be fetched from the API
+   * @var array
+   */
+  protected $fetch = array(
+    "supertags",
+    "file"
+  );
+
   public function __construct($deps = array())
   {
     $this->api = isset($deps["api"]) ? $deps["api"] : new \WPUtilities\API();
-    $this->contentTypes = isset($deps["contentTypes"]) ? $deps["contentTypes"] : new \WPUtilities\ACF\ContentTypes();
+    $contentTypes = isset($deps["contentTypes"]) ? $deps["contentTypes"] : new \WPUtilities\ACF\ContentTypes();
+    $this->contentTypes = $contentTypes->find();
   }
 
   public function getPost($id)
   {
-    return $this->api->get("/{$id}")->data;
+    $data = $this->api->get("/{$id}")->data;
+    return $this->parseFields($data);
+  }
+
+  /**
+   * Loop through all the fields and modify
+   * the value, if necessary.
+   *
+   * Currently transforming API URLs into
+   * the data at that endoiint
+   * @param  object $post Post
+   * @return obejct Modified post
+   */
+  protected function parseFields($post)
+  {
+    $fields = $this->contentTypes["page"];
+
+    foreach ($post->meta as $key => $value) {
+
+      $field = $fields[$key];
+
+      // no value or (not a repeater and not a fetch field)
+      if (empty($value) || ($field["type"] != "repeater" && !in_array($field["type"], $this->fetch))) continue;
+
+      if ($field["type"] == "repeater" && count($field["sub_fields"]) > 1) {
+        
+        $children = array();
+
+        foreach ($field["sub_fields"] as $kid) {
+          $children[$kid['name']] = $kid;
+        }
+
+        foreach ($value as $row) {
+
+          foreach ($row as $rowField => &$fieldValue) {
+              if (!in_array($children[$rowField]["type"], $this->fetch)) continue;
+              $fieldValue = $this->fetch($fieldValue);
+              continue;
+          }
+
+          
+        }
+
+      } else {
+        $post->meta->$key = $this->fetch($value);
+      }      
+
+    }
+
+    return $post;
+  }
+
+  protected function fetch($value)
+  {
+    if (is_array($value)) {
+      $value = array_map(function ($url) {
+        return $this->api->get($url)->data;
+      }, $value);
+    } else {
+      $value = $this->api->get($value)->data;
+    }
+
+    return $value;
   }
 
 }
--- src/Theme/Transformers/Page.php
+++ src/Theme/Transformers/Page.php
@@ -7,9 +7,6 @@ class Page extends Base
   public function __construct($deps = array())
   {
     parent::__construct();
-
-    $supertags = $this->contentTypes->findSupertags();
-    $this->supertags = $supertags["page"];
   }
 
   public function parseRegions($post)
@@ -23,8 +20,6 @@ class Page extends Base
         continue;
       }
 
-      $value = $this->compileSupertags($key, $value);
-
       $region = $matches[1];
       $field = $matches[2];
 
@@ -34,59 +29,6 @@ class Page extends Base
     }
 
     return $post;
-  }
-
-  protected function compileSupertags($key, $value)
-  {
-    if (!$value) return $value;
-    if (!isset($this->supertags[$key])) return $value;
-
-    $supertag = $this->supertags[$key];
-
-    if (isset($supertag["children"])) {
-
-      // this is a repeater field with supertags as children
-
-      $children = $supertag["children"];
-      $childFieldNames = array_keys($children);
-
-      if (count($children) == 1) {
-
-        // this repeater only has one child. When we first got the
-        // meta, we pushed the values out of the subfield array into
-        // the main array. So now we need to loop through the main value
-
-        foreach ($value as &$v) {
-          $v = $this->getDataFromApi($v);
-        }
-          
-      } else {
-
-        foreach ($value as &$row) {
-
-          foreach ($row as $subFieldName => &$v) {
-            // not a supertag field
-            if (!in_array($subFieldName, $childFieldNames)) continue;
-            $v = $this->getDataFromApi($v);
-          }
-
-        }
-
-      }
-
-    } else {
-
-      // top-level supertag
-      
-      $value = $this->getDataFromApi($value);
-    }
-
-    return $value;
-  }
-
-  protected function getDataFromApi($url)
-  {
-    return $this->api->get($url)->data;
   }
 
 }
