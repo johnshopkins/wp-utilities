3163846f3e0ed231d1a02c32b80c7fd63f013512
Jen Wachter <jenleighkelly@gmail.com>
2014-06-20 13:13:54 -0400

    Sent back null if the value is an empty string.

--- src/ACF/Fields/jhu_color_picker.php
+++ src/ACF/Fields/jhu_color_picker.php
@@ -13,9 +13,12 @@ class jhu_color_picker extends Base
   {
     $value = parent::getValue($meta);
 
-    // $value is an object, which empty() does not work
+    if (is_string($value) && empty($value)) return null;
+
+    // $value could be an object, which empty() does not work
     // on. Cast as an array and then check value.
     $asArray = (array) $value;
+
     if (empty($asArray)) return null;
 
     return $value;
