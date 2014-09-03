ad3be3aa09677e0ea2c609cb7ddf2c70972392f7
Jen Wachter <jenleighkelly@gmail.com>
2014-09-03 12:20:55 -0400

    Added taxonomy field.

---
+++ src/ACF/Fields/taxonomy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace WPUtilities\ACF\Fields;
+
+class taxonomy extends Base
+{
+  protected $taxonomy;
+
+  public function __construct($fieldData, $id, $parent = null, $deps = array())
+  {
+    parent::__construct($fieldData, $id, $parent, $deps);
+    $this->taxonomy = $fieldData["taxonomy"];
+  }
+
+  protected function getValue($meta)
+  {
+    $value = parent::getValue($meta);
+    if (empty($value)) $value = array();
+
+    return array_map(function ($id) {
+      $term = $this->wordpress->get_term($id, $this->taxonomy);
+      return array(
+        "name" => $term->name,
+        "slug" => $term->slug
+      );
+    }, $value);
+  }
+
+}
