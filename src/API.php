067ca599f7bdc2bc667df945c990c91bb552601e
Jen Wachter <jenleighkelly@gmail.com>
2015-05-01 11:59:49 -0400

    Redrafted base URL based on API settings.

--- src/API.php
+++ src/API.php
@@ -29,16 +29,26 @@ class API
     return "https://{$prefix}.jhu.edu/api";
   }
 
+  /**
+   * If this is an absolute URL, get rid of everything up to /api and
+   * recraft the base URL. This is necessary when the requested endpoint
+   * is https://beta, but the request needs to be made to https://origin-beta
+   * because the API has been set to admin mode.
+   */
+  protected function removeBaseUrl($endpoint)
+  {
+    return preg_replace("/^(.*?)\.jhu\.edu\/api/", "", $endpoint);
+  }
+
   public function get($endpoint, $params = array(), $headers = array(), $options = array())
   {
-    if (substr($endpoint, 0, strlen($this->apiBase)) != $this->apiBase) {
+    $endpoint = $this->removeBaseUrl($endpoint);
 
-      if (substr($endpoint, 0, 1) != "/") {
-        $endpoint = "/{$endpoint}";
-      }
-
-      $endpoint = $this->apiBase . $endpoint;
+    if (substr($endpoint, 0, 1) != "/") {
+      $endpoint = "/{$endpoint}";
     }
+
+    $endpoint = $this->apiBase . $endpoint;
 
     return $this->http->get($endpoint, $params, $headers, $options)->getBody();
   }
